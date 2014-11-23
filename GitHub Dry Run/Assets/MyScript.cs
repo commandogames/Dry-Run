b3fe0ceeee3107b804f49755cbf357cc93760882
Johndale Alfred Julian <j.julian@klab.com>
2014-11-23 13:01:56 +0800

    Testing

--- GitHub Dry Run/Assets/MyScript.cs
+++ GitHub Dry Run/Assets/MyScript.cs
@@ -6,13 +6,30 @@ public class MyScript : MonoBehaviour {
 
     Color myColor;
     float myTimer;
+    public LayerMask[] myLayers;
+    public float layerIndex;
+
 	// Use this for initialization
 	void Start () {
-	
+        //int i = 0;
+        foreach (Transform child in transform)
+        {
+            
+        }
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            for (int j = 0; j < layerIndex; j++)
+            {
+                print(i);
+                transform.GetChild(i).gameObject.layer = 8 + j;
+            }
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
 
         myColor = renderer.material.color;
@@ -22,7 +39,8 @@ public class MyScript : MonoBehaviour {
         myTimer = (myTimer > 4) ? myTimer = 0 : myTimer += Time.deltaTime;
 
         renderer.material.color = myColor;
-        
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * 2.0f, 0, Input.GetAxis("Vertical") * 2.0f);
 
 	}
 }
